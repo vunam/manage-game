a73e167655acb0ed7c059d7b78d2011d0df34193
Vu Nam Nguyen <vu@strangelab.co.uk>
2018-08-04 19:12:23 +0100

    Add working client integration

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,3 +1,10 @@
 import * as React from 'react';
+import { render } from 'react-dom';
+import Main from './containers/Main';
 
-console.log('xxx')
+render(
+  <div>
+    <Main />
+  </div>,
+  document.getElementById('root'),
+);
